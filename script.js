abd9bebf3cd284bc25fa37a903bbdc5a45086754
Anon UNO Intercongreso Contoni <intercongreso3@gmail.com>
2024-02-21 14:00:27 -0400

    Actualización del contenido en el archivo index.html

---
+++ script.js
@@ -0,0 +1,5 @@
+const boton = document.createElement("<button id=stop18>stop</button>
+<p id="texttospeech">");
+boton.innerHTML = "stop";
+boton.style = "bottom:10px;right:10px;position:fixed;z-index:9999;"
+document.body.appendChild(boton);
